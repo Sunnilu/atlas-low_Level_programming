9ca0ccdf024eff178909cfdd4392b50298651068
root <Suntha.Lucas@atlasschool.com>
2024-01-20 22:06:56 +0000

    9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-    putchar('0' + 0);
-    putchar(',');
-    putchar(' ');
-    putchar('0' + 1);
-    putchar('\n');
+    int x;
 
+    for (x = 0; x < 10; x++)
+    {
+        putchar(x + '0');
+        if (x < 9)
+        {
+            putchar(',');
+            putchar(' ');
+        }
+    }
+    putchar('\n');
     return (0);
 }
 
