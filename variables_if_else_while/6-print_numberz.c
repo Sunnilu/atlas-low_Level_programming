2bf3edcec685f40017bbd67e72f2ea221f851755
root <Suntha.Lucas@atlasschool.cm>
2024-01-20 02:12:03 +0000

    6

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -7,18 +7,9 @@
  */
 int main(void)
 {
-	putchar('0' + 0);
-	putchar('0' + 1);
-	putchar('0' + 2);
-	putchar('0' + 3);
-	putchar('0' + 4);
-	putchar('0' + 5);
-	putchar('0' + 6);
-	putchar('0' + 7);
-	putchar('0' + 8);
-	putchar('0' + 9);
+    putchar('0' + 0);
+    putchar('\n');
 
-	putchar('\n');
+    return (0);
+}
 
-	return (0);
-}	
