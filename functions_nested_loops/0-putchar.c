23c386475519e1b6a6f1a396d48a5a7ae056b151
root <Suntha.Lucas@atlasschool.com>
2024-01-21 21:30:42 +0000

    0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,6 +1,19 @@
-#include <stdio.h>
-
-int main(void) {
-    printf("_putchar\n");
+#include "main.h"
+#include <stdio.h
+>
+**
+* main - entry_point
+* return: always 0
+	* /
+ int main(void)
+{
+   _putchar('_');
+   _putchar('p');
+   _putchar('u');
+   _putchar('t');
+   _putchar('c');
+   _putchar('h');
+   _putchar('a');
+   _putchar('r');
     return 0;
 }
