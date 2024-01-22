f14d99a9e3bad9993a5396bb15dbf27bd6439e3a
root <Suntha.Lucas@atlasschool.com>
2024-01-22 21:05:12 +0000

    0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+    /* Your implementation of _putchar goes here */
+
+int my_putchar(void) {
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+    return 0;
+}
+
+int main() {
+    return my_putchar();
+}
+
