827230c46045db971c95a15c88a2f8ce2d778126
root <Suntha.Lucas@atlasschool.com>
2024-01-22 17:09:52 +0000

    0

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,8 +1,8 @@
-int _putchar(char c).
+int _putchar(char c);
 void print_alphabet(void);
 void print_to_98(int n);
 void times_table(void);
-void print_alphabet_x10(void)
+void print_alphabet_x10(void);
 int _abs(int);
 void print_times_table(int n);	
 int add(int, int);
@@ -10,4 +10,3 @@ int print_last_digit(int);
 int print_sign(int n);
 int _isalpha(int c);
 int _islower(int c);
-void print_times_table(int n);
