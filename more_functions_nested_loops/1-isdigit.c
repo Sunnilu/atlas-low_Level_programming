1b2fe444d2c5ab03cbb1d539a7d5e5fca1ae99d1
Suntha <Suntha.Lucas@atlasschool.com>
2024-01-24 22:44:29 +0000

    1

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,5 +1,6 @@
-int _isdigit(int c) {
-    /* check if the character is a digit */
-    return (c >= '0' && c <= '9') ? 1 : 0;
+int _isdigit(int c)
+{
+  /* check if the character is a digit */
+     return (c >= '0' && c <= '9') ? 1 : 0;
 }
 	
