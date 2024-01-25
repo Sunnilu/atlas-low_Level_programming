30152caac9849a23b69c228bf4b7bba3063d2764
Suntha <Suntha.Lucas@atlasschool.com>
2024-01-25 02:54:37 +0000

    4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * 'print_most_numbers' - function that prints the numbers from 0 to 9, except for the number 2
+ * 'print_most_numbers' - function that prints the numbers from 0 to 9, except for the numbers 2 and 4
  * Return: 0
  */
 void print_most_numbers(void)
@@ -10,7 +10,7 @@ void print_most_numbers(void)
 
     for (c = 48; c < 58; c++)
     {
-        if (c != 50)
+        if (c != 50 && c != 52)
         {
             _putchar(c);
         }
