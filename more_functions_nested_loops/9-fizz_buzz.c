d4b295e777d85ed69d0f46091445d6eeb7561da7
Suntha <Suntha.Lucas@atlasschool.com>
2024-01-26 00:42:17 +0000

    9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    for (int i = 1; i <= 100; i++) {
-        if (i % 3 == 0 && i % 5 == 0) {
+    for (int i = 1; i < 100; i++) {
+        if (i % 15 == 0) {
             printf("FizzBuzz ");
         } else if (i % 3 == 0) {
             printf("Fizz ");
@@ -12,10 +12,6 @@ int main() {
             printf("%d ", i);
         }
     }
-
-    printf("\n");
-
     return 0;
 }
-
-      
+ 
