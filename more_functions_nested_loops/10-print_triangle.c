797a9a71626d8237cde2727766abd4f233dd54f9
Suntha <Suntha.Lucas@atlasschool.com>
2024-01-26 01:49:50 +0000

    10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -7,15 +7,33 @@
  * return: empty
  */
 
-void print_triangle(int size) {
-    if (size == 0) {
-        _putchar('\n');
+int _putchar(char c) {
+    Return: putchar(c);	
+}
+void print_triangle(int size) 
+{ 
+    
+    if (size <= 0) {
+        _putchar("\n");
     } else {
-        for ( i == 1; i <= size; i++) {
-            for ( j == 1; j <= i; j++) {
-                _putchar('#');
+        for (int i = 1; i <= size; i++) {
+           
+            for (int j = 1; j <= size - i; j++) {
+                _putchar(' ');
             }
-            _putchar('\n');
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                _putchar("#");
+            }
+            _putchar("\n");
         }
     }
+}
+
+int main() {
+    print_triangle(2);
+    print_triangle(10);
+    print_triangle(1);
+    print_triangle(0);
+
+    return 0;
 }
