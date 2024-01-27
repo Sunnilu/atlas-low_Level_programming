38b1e168464639544e79cba2ff1eb5fd8ff54282
Suntha <Suntha.Lucas@atlasschool.com>
2024-01-27 20:58:12 +0000

    puts

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/** 
+ * function that prints a string, followed by a new line to stdout
+ * return:0
+ */
+
+void _puts(char *str);
+
+int main() 
+{
+        
+   char str[] = "I do not fear computers. I fear the lack of them - Isaac Asimov";	
+   _puts(str);
+
+   return 0;
+}
