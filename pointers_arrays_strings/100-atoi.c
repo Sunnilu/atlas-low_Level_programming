942841d0f34b3ae274c10f6316ab3d19821b430d
Suntha <Suntha.Lucas@atlasschool.com>
2024-01-28 22:38:26 +0000

    convert

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,22 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * a function that converts a string to an anteger
- * Return: 0
+ * write a function that convert a string to an integer
+ * not allowed to use long
+ * return: 0
  */
 
 int _atoi(char *s);
 
-int main() {
-    int nb;
-
-   
-    nb = _atoi(" -98 Battery Street; San Francisco, CA 94111 - USA");
+int main(void)
+{
+	int nb;
+	
+;	nb = _atoi("98");
     printf("%d\n", nb);
-
+    nb = _atoi("-402");
+    printf("%d\n", nb);
+    nb = _atoi("          ------++++++-----+++++--98");
+    printf("%d\n", nb);
+    nb = _atoi("214748364");
+    printf("%d\n", nb);
+    nb = _atoi("0");
+    printf("%d\n", nb);
+    nb = _atoi("Suite 402");
+    printf("%d\n", nb);
+    nb = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
+    printf("%d\n", nb);
     nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
     printf("%d\n", nb);
-
-    return 0;
-}    
+	
+	return 0;
+}	
