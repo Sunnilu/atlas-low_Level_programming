693594896ac27c1eb35805b361469943bd295ec7
Suntha <Suntha.Lucas@atlasschool.com>
2024-01-28 00:52:03 +0000

    reverse

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,19 +1,20 @@
 #include "main.h"
 #include <stdio.h>
-#include <string.h>
+
 /**
  * prints a string, in reverse, followed by a new line
- * return: 0
+ * @s: the string to print
+ * return: void
  */
 
-void print_rev(char *s); 
+void print_rev(char *s);
 
-int main() {
-	char str[50];
-	printf("\n \"I do not fear computers. I fear the lack of them - Isaac Asimov");
-	scanf("%s", str);
+int main() 
+{
+    
+    char str[] = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+    
+    print_rev(str);
 
-	print_rev(str);
-		
-	return 0;	
-}	
+    return 0;
+}			
