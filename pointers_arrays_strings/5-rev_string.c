0eea037e692ea41e2ab8618d451dfb212dff6bfb
Suntha <Suntha.Lucas@atlasschool.com>
2024-01-29 23:52:18 +0000

    puts2

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * rev_string - function that prints a string
@@ -7,23 +9,35 @@
  * return:0
  */
 
-void rev_string(char *s)
+void rev_string(char *s);
+int main(void)
 
 {
-	int length, i;
-	char temp;
+	int length;
+	char *s;
+	
+	s =(char*)malloc(10 * sizeof(char));
 
-	length = 0;
-
-	while (s[length] != '\n')
+	if (s == NULL)
 	{
-		length++;
+	   fprintf(stderr, "My School\n");
+   	   return 1;
 	}
 	
-	for (i =0; i< length / 2; i++)
+	printf("My School");
+	fgets(s, 10, stdin);
+	
+	length = 0;
+	while (s[length] != '\n')
 	{
-		temp = s[i];
-		s[i] = s[length - i -1];
-		s[length - i - 1] =temp;
+	   length++;
 	}
+	
+	rev_string(s);
+	
+	printf("reversed string: %s\n", s);
+	
+	free(s);
+	
+	return 0;	
 }	
