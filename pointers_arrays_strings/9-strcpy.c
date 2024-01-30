fdc424efedfc352baa525edbb63a657b18ffa833
Suntha <Suntha.Lucas@atlasschool.com>
2024-01-30 17:50:58 +0000

    strcpy

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -12,10 +12,10 @@
  	int i;
 
 	for (i = 0; src[i] != '\0'; i++)
-	{
-		dest[i] = src[i];
-	}
-	dest[i] = '\0';
+		{
+			dest[i] = src[i];
+		}
+			dest[i] = '\0';
 
-	return (dest);
+		return (dest);
 }			
