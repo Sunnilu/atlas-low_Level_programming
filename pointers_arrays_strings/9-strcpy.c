a6d2cee632b9cd8f06ab68753e9db665259f59b9
Suntha <Suntha.Lucas@atlasschool.com>
2024-01-30 17:55:16 +0000

    strcpy

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -6,10 +6,10 @@
  * @scr: string 2 pointer to a char
  * return: character (dest)
  */
- 
- char *_strcpy(char *dest, char *src)
- {
- 	int i;
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
 
 	for (i = 0; src[i] != '\0'; i++)
 		{
@@ -18,4 +18,4 @@
 			dest[i] = '\0';
 
 		return (dest);
-}			
+}		
