9a52a912f0e62a771a8d0c86d12f93a7e929a37f
Suntha <Suntha.Lucas@atlasschool.com>
2024-01-31 17:45:04 +0000

    headers

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * writes a function that concatenates two strings
+ * @:src
+ * @:dest
+ * return: (\0)
+ */
+
+char *_strcat(char *dest, char *src)
+int main()
+
+	char* my_strcat(char *dest, char *src) {
+		while (*dest) {
+			dest++;
+		}		
+		while (*src) {
+			*dest = *src;
+			dest++;
+			src++;
+		}
+		*dest = '\0';
+
+		return dest;
+		}
+
+		char s1[98] = "Hello";
+		char s2[] = "World";
+		char *ptr = my strcat(s1, s2);
+
+		printf("%s\n", s1
+		
+		return 0;
+}				
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,17 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
 
-int _putchar(char c);
-void swap_int(int *a, int *b);
-void reset_to_98(int *n);
-int _strlen(char *s);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-char *_strcpy(char *dest, char *src);
-void print_array(int *a, int n);
-int _atoi(char *s);
+char *_strcat(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+char *_strncpy(char *dest, char *src,
+char *_strncat(char *dest, char *src, int n);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
 
 #endif /* MAIN_H */
