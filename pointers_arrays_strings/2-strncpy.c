c8b17ce4e2a5558d1d28a9f6878d9dfcc93728a8
Suntha <Suntha.Lucas@atlasschool.com>
2024-02-01 01:56:24 +0000

    headers

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -14,7 +14,7 @@ char *_strncpy(char *dest, char *src, int n)
 
 {
 	int index = 0, src_len = 0; 
-
+	
 	while (src[index++])
 	src_len++;
 	
@@ -22,6 +22,7 @@ char *_strncpy(char *dest, char *src, int n)
 	dest[index] = src[index];
 	for (index = src_len; index < n; index++)
 	dest[index] = src[index];
+	
 
 	return (dest);
 }
