74c9c7e9dfc8cb602f9f25bd7b461de6b1247aaf
Suntha <Suntha.Lucas@atlasschool.com>
2024-02-01 16:12:29 +0000

    cap

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * cap_string - capitalizes all words of the string
@@ -7,16 +8,23 @@
  * return: a pointer to the changed string
  */
 
-char *cap_string(char *str);
 
-int main(void)
-{
-    char sentence[] = "this is a test sentence.";
-    printf("Original: %s\n", sentence);
-    
-    cap_string(sentence);
-    
-    printf("Capitalized: %s\n", sentence);
+      
+char *cap_string_(char *str) {
+ 
 
-    return 0;
-}  
+    int index  = 0;
+    while (str[index]) {   
+
+    while (!(str[index] >= 'a' && str[index] <= 'z'))
+       index++;
+        
+    if (str[index - 1] == ' ' || str[index - 1] == '\t' || str[index - 1] == '\n' ||
+             str[index - 1] == ',' || str[index - 1] == ';' || str[index - 1] == '.' ||
+             str[index - 1] == '!' || str[index - 1] == '?' || str[index - 1] == '"' ||
+             str[index - 1] == '(' || str[index - 1] == ')' || str[index - 1] == '{' ||
+             str[index - 1] == '}' || index == 0) str[index] -= 32; }
+             index++;
+
+    return (str);
+}
