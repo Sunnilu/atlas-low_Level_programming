b7b9f96e26925dca0ef74301e286e3cf452f1b19
Suntha <Suntha.Lucas@atlasschool.com>
2024-02-02 18:01:10 +0000

    headers

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+gcc main.c cap_string.c -o program
 
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -10,7 +11,7 @@ char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
-char *cap_string(char *);
-char *leet(char *);
+char *cap_string(char *str);
+char *leet(char *str);
 
 #endif /* MAIN_H */
