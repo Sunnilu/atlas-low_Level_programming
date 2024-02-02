d108c9e7d9677f08b89f98f181e1510a28d563ad
Suntha <Suntha.Lucas@atlasschool.com>
2024-02-02 17:22:08 +0000

    7-1337

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * encode into 1337
+ * @n: input value
+ * return: n value
+ */
+char *leet(char *str) {
+    int i = 0;
+    str [i] = '\0';   	
+    	
+        {
+        if (str[i] == 'a' || str[i] == 'A') {
+            str[i] = '4';
+        } else if (str[i] == 'e' || str[i] == 'E') {
+            str[i] = '3';
+        } else if (str[i] == 'o' || str[i] == 'O') {
+            str[i] = '0';
+        } else if (str[i] == 't' || str[i] == 'T') {
+            str[i] = '7';
+        } else if (str[i] == 'l' || str[i] == 'L') {
+            str[i] = '1';
+        }
+        i++;
+    }	
+
+    return str;
+}    
