76afdc8521b0d9c352c667b8ef0c33ab1bf86d6f
Suntha <Suntha.Lucas@atlasschool.com>
2024-02-03 19:29:54 +0000

    strstr

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -14,7 +14,7 @@ char *_strstr(char *haystack, char *needle)
 	{
 		char *one = haystack;
 		char *two = needle;
-		
+
 		while (*one == *two && *two != '\0')
 		{
 			one++;
@@ -24,4 +24,4 @@ char *_strstr(char *haystack, char *needle)
 			return (haystack);
 	}
 	return (NULL);
-}		
+}
