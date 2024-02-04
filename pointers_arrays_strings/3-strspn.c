01e1cd704dfaec95e65c6259d660d171bfb9214f
Suntha <Suntha.Lucas@atlasschool.com>
2024-02-04 21:09:51 +0000

    strspn

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -10,27 +10,25 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, n, value, check;
-
 	value = 0;
 
-	    for (i = 0; s[i] != '\0'; i++)
-	    {
+	for (i = 0; s[i] != '\0'; i++)
+	{
 		check = 0;
-
-            for (n = 0; accept[n] != '\0'; n++)
-	    {
+	for (n = 0; accept[n] != '\0'; n++)
+		{
 		if (accept[n] == s[i])
 	{
-		value++;
-		check = 1;
-	    }
-        }
-       		 if (check == 0)
-       	{
-            return value;
-        }
-    }
+			value++;
+			check = 1;
+	}
+	}
+		if (check == 0)
+		{
+		return value;
+	}
+	}
 
-    return value;
+	return value;
 }
 
