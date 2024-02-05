99421cd2c6663d02154b28c89ed8ad3294fe7df2
Suntha <Suntha.Lucas@atlasschool.com>
2024-02-05 02:17:01 +0000

    times table

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 int _putchar(char c);
 void print_alphabet (void);
 void print_to_98(int n);
-void time_table(void);
+void times_table(void);
 void print_alphabet_x10(void);
 int abs(int);
 void print_times_table(int n);
