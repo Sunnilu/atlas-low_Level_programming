49a1d3ef88ae64f553f60c8f0b3ed66ff0b043cc
Suntha <Suntha.Lucas@atlasschool.com>
2024-02-09 01:46:28 +0000

    headers

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,41 +1,30 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * _sqrt_recursion that returns the natural square root of a number
- * @n: the natural square root
- * return: square root function should return -1
+ * sqrt_a  - return the natural square root of a number
+ *
+ * @n: input number
+ * return: square root or  -1
  */
 
-int check_square(int x, int n)
+int sqrt_a(int a, int b)
 {
-	if (x * x == n) {
-		return 1;
-	}
-	if (x * x > n)
+	if (b * b == a)
 	{
-		return 0;
+		return (b);
 	}
-	return check_square(x + 1, n);
+	else if (b * b > 4)
+	{
+		return (-1);
+	}
+	return (sqrt_a(a, b + 1));
 }
-
+/** 
+ * _sqrt_ecursion - returns the natural square root of a number
+ * @n: the inmput number
+ * return: natural square root
+ */
 int _sqrt_recursion(int n)
-	{
-	return check_square(0, n);
-}
-
-int main()
 {
-	int number = 25;
-
-	int result = _sqrt_recursion(number);
-
-	if (result == 1)
-	{
-	printf("the number %d has a natural square root.\n",number);
-	}
-	else
-	{
-	printf("the number %d does not have a natural square root,\n", number);
-	}
-	return (0);
+	return (sqrt(n, 0));
 }
