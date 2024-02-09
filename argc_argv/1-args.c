f9138000b67009e6df24302f618f3161c40e2eaf
Suntha <Suntha.Lucas@atlasschool.com>
2024-02-09 23:59:55 +0000

    args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * number of arguments passed
+ * @argc: count arguments
+ * @argv: argument vector
+ * return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	 printf("%d\n", argc - 1);
+	 return(0);
+}
