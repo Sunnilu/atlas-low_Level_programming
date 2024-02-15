609fb9011ff66c2415b57f820ed17597ac81ec1f
Suntha <Suntha.Lucas@atlasschool.com>
2024-02-15 18:04:43 +0000

    malloc_free

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * create_array of chars and initializes with specific char, return a pointer to the array or Null if it fails. unsigned int size
+ * create_array of chars and initializes with specific char, unsigned int size
  * @c: the specific char
  * @size: the size of the the memory to print
  * return: NULL if size is 0
