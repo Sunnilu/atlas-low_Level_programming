164aee47591ae2f6a07b6c5f679855314fa61cd8
Suntha <Suntha.Lucas@atlasschool.com>
2024-02-15 23:10:00 +0000

    malloc_checked

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -5,18 +5,17 @@
 /**
  * malloc_checked - allocates memory
  * @b: the size allocated
- * 
  * return: 0
  */
 
 void *malloc_checked(unsigned int b)
 {
 	void *ptr;
-	
+
 	ptr = malloc(b);
 	
 	if (ptr == NULL)
 		exit(98);
 	
-	return ptr;
+	return (ptr);
 }
