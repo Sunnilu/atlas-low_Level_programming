829517beab59da4a690ba030da76bdf3c17f6d28
Suntha <Suntha.Lucas@atlasschool.com>
2024-02-21 16:03:17 +0000

    header

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * define a new type
+ * @char: pointer
+ * @float: used with age
+ * @owner: pointer
+ * return: 0
+ */
+
+struct dog
+{
+	char* name;
+	float age;
+	char* owner;
+};
--- structures_typedef/main.h
+++ structures_typedef/main.h
@@ -3,7 +3,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdlib.h>
-#include guard
+#include <guard.h>
  
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
