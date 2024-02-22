e8c16d434efe2776da422d3a5e2be1fdf0ff1c4b
Suntha <Suntha.Lucas@atlasschool.com>
2024-02-22 19:10:10 +0000

    new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include "dog.h"
-#define MAIN_H
-#ifndef MAIN_H
+#include <string.h>
 
 
 /**
@@ -71,6 +70,24 @@ int _strlen(char *s)
 	}
 	return (k);
 }
-#endif
+
+/**
+ * @dest: destination value
+ * @src: source value
+ * return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	dest[i++] = '\0';
+	
+	return (dest);
+}
 
 
