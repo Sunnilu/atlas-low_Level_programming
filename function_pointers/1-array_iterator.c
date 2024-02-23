6f9b24ad889ed215395df94a7916b6c5eb81ef6c
Suntha <Suntha.Lucas@atlasschool.com>
2024-02-23 15:39:53 +0000

    arrayiterator

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -5,7 +5,7 @@
  * array_iterator - executes a function given as a parameter
  * @size: the size of the array
  * @array: the place to store the stuff in a row
- * @action: a pointer to the function 
+ * @action: a pointer to the function
  * return: nothing
  */
 
@@ -16,7 +16,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	if (array != NULL && action != NULL && size > 0)
 	{
 		while (j < size)
-		{	
+		{
 			action(array[j]);
 			j++;
 		}
