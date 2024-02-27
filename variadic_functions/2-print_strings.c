80041bd0bd9e510932d4898d4ea22b2968654a1a
Suntha <Suntha.Lucas@atlasschool.com>
2024-02-27 00:34:38 +0000

    print_strings

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_strings - prints string passed to the function
+ * @separator: a string that separates the string
+ * @n: unnamed parameters
+ * return :0
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	char *str;
+	unsigned int i;
+	va_list ap;
+
+	va_start(ap, n);
+	if (separator == NULL)
+		separator = "";
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(ap, char*);
+		if (str == NULL)
+			str = "[nil]";
+		printf("%s", str);
+		if (i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ap);
+}
