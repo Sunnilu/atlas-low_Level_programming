d08b67546ce6afdb7ebb4f15ab04b329f1bba33e
Suntha <Suntha.Lucas@atlasschool.com>
2024-03-06 23:32:58 +0000

    lists.h

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -14,6 +14,12 @@
  * @next: points to the next node
  * description: singly linked list node structure
  */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
