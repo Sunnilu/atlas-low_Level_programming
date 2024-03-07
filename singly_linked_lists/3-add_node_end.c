a093489fc337c6c9b9d9284043f19618dd232283
Suntha <Suntha.Lucas@atlasschool.com>
2024-03-07 22:11:45 +0000

    add_node_end

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -12,20 +12,23 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	size_t n,
+	list_t *new_node, *current_node;
+	size_t n;
 	
 	new_node = malloc(sizeof(list_t));
-	if (add_node == NULL)
+
+	if (new_node == NULL)
 		return (NULL);
+
 	new_node->str = strdup(str);
 
 	for (n = 0; str[n]; n++);
 
 	new_node->len = n;
 	new_node->next = NULL;
-	add_node->*head;
+	current_node = *head;
 
-	if (current_node == NULL)
+	if (*head == NULL)
 	{
 		*head = new_node;
 	}
