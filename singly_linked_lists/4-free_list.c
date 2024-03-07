416010bed1943e7bc80c16186a496ebe3f1ded47
Suntha <Suntha.Lucas@atlasschool.com>
2024-03-07 22:33:40 +0000

    free_list.c

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * free_list - frees a list
+ * @head: head of the linked list
+ * return: nothing
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current_node;
+
+	while ((current_node = head) != NULL)
+	{
+		head = head->next;
+		free(current_node->str);
+		free(current_node);
+	}
+}
