a56edd4504d7cb95b44d5e0493ab154666a349b0
Suntha <Suntha.Lucas@atlasschool.com>
2024-03-08 23:15:30 +0000

    free listint2.c

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - a function that frees a listint_t list
+ * @head: a double pointer to the list
+ * return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *next;
+
+	if (head == NULL)
+		return;
+	while (*head != NULL)
+	{
+		next = (*head)->next;
+		free(*head);
+		*head = next;
+	}
+}	
