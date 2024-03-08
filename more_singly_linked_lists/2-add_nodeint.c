b7b505ba12aea40e81bccd4d7842740a12d52a41
Suntha <Suntha.Lucas@atlasschool.com>
2024-03-08 22:03:37 +0000

    add nodeint.c

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -5,36 +5,24 @@
 /**
  * add_nodeint - adds a new node at the beginning of a listint_t list
  * @h: head of the pointer point to the head of the list
- * @n: data to be added to the new list
+ * @n: integer value to be added to the new node
  * return: the address of the new element or NULL if it fails
  */
 
 listint_t *add_nodeint(listint_t **head, const int n)
-{
-	listint_t *new;
-	listint_t *temp;
-
-	(void)temp;;
-
-	new =malloc(sizeof(listint_t));
-	if (new == NULL)
+{	
+	listint_t *ptr;
+	
+	if (head == NULL)
 		return (NULL);
 
-	new->n = n;
-	new->next = NULL;
-	temp = *head;
-	if (*head == NULL)
-	{
-		*head = new;
-	}
-	else
-	{
-		while ( temp->next != NULL)
-		{
-			temp = temp->next;
-		}
-		temp->next = new;
-	}
-	return (*head);
-}
+	ptr  = malloc(sizeof(listint_t));
+	if (ptr == NULL)
+		return (NULL);
 
+	ptr->n = n;
+	ptr->next = *head;
+	*head = ptr;
+
+	return (ptr);
+}
