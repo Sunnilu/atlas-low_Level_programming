6b767e944703c09e222b318d9b45e08ab594f4a3
Suntha <Suntha.Lucas@atlasschool.com>
2024-03-08 00:40:56 +0000

    print_lintint.c

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -9,10 +9,10 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t count;
-	
+
 	if (h == NULL)
 		return (0);
-	for ( count = 0; h != NULL; count++)
+	for (count = 0; h != NULL; count++)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
