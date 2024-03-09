e65f5a18ca2f90e1f9a4fabb9220826ce296fd77
Suntha <Suntha.Lucas@atlasschool.com>
2024-03-09 13:48:12 +0000

    insert nodeint.c

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 
 /**
- * insert_nodeint_at_index - a function that inserts a new node at a given 
- * position
+ * insert_nodeint_at_index - a function that inserts a new node
  * @headL double pointer
  * @idx: index of the node
  * returnL the address of the new node
@@ -26,7 +25,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			return (NULL);
 	}
 	new = malloc(sizeof(listint_t));
-	if ( new == NULL)
+	if (new == NULL)
 		return (NULL);
 	new->n = n;
 	if (idx == 0)
