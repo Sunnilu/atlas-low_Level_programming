4c91e3397ea8206575e37f67af37c66486259f9a
Suntha <Suntha.Lucas@atlasschool.com>
2024-03-10 00:18:58 +0000

    get bit.c

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * get_bit - a function that returns the value of a bit at a given index
+ * @n: unsigned integer
+ * @index: the start of the bit
+ * return: the value of the bit at the index or 1 if error occured
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int divisor, check;
+
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+			return (-1);
+	divisor = 1 << index;
+	check = n & divisor;
+	if (check == divisor)
+		return (1);
+	return (0);
+}
