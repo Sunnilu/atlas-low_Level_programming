e2c924ba7f859b865bc87c3dbbfd138795fd6053
Suntha <Suntha.Lucas@atlasschool.com>
2024-03-12 03:05:47 +0000

    append text to file.c

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -8,14 +8,15 @@
 /**
  * append_text_to_file - a function that appends text at the end of a file
  * @filename: the name of the file
- * @text_content: the NULL terminated string to add at the end of the file
- * return: if NULL, return -1
+ * @text_content: the content to be appended to the file
+ * return: 1 on success and -1 if not
  */
 
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t bytes_written;
+	int nletters;
+	int rwr;
 
 	if (!filename)
 	{
@@ -23,43 +24,29 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 
 	fd = open(filename, O_WRONLY | O_APPEND);
+
 	if (fd == -1)
 	{
 		return (-1);
 	}
-	if (text_content == NULL)
+	if (text_content)
 	{
-		close(fd);
-		return (1);
-	}
+		for (nletters = 0; text_content[nletters]; nletters++);
 
-	bytes_written = write(fd, text_content, strlen(text_content));
-	if (bytes_written == -1)
-	{
-		close(fd);
-		return (-1);
+		rwr = write(fd, text_content, nletters);
+
+		if (rwr == -1)
+			return (-1);
 	}
+		
+		close(fd);
+
+		return (-1);
+	
 
 	if (close(fd) == -1)
 	{
 		return (-1);
 	}
 	return (1);
-}
-int main()
-{
-	const char *filename = "example.txt";
-	char text_content[] = "this is the text to append";
-
-	int result = append_text_to_file(filename, text_content);
-	if (result == 1)
-	{
-		printf("Text appended successfully.\n");
-	}
-	else
-	{
-		printf("Failed to append text.\n");
-	}
-
-	return (0);
 }
