d5d90a8fe61dbd26b5dd72d91070a0c1da638572
Suntha <Suntha.Lucas@atlasschool.com>
2024-03-16 02:22:51 +0000

    shash.h

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,27 +1,30 @@
 #include "hash_tables.h"
+#include <stdlib.h>
 
 /**
  * hash_table_create - creates a new hash table
  * @size: the size of the array
  *
- * return: if an error occurs - NULL,  a pointer to the newly created hash table
+ * return: if an error occurs - NULL, otherwise a pointer to the new hash table
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht;
-	unsigned long int i;
-
-	ht = malloc(sizeof(hash_table_t));
-	if (ht == NULL)
+	hash_table_t *table;
+	
+	if (size == 0)
+		return (NULL);
+	
+	table = calloc(1, sizeof(hash_table_t));
+	if (table == NULL)
 		return (NULL);
 
-	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t *) * size);
-	if (ht->array == NULL)
+	table->size = size;
+	table->array = calloc(size, sizeof(hash_node_t *));
+	if (table->array == NULL)
+	{
+		free(table);
 		return (NULL);
-	for (i = 0; i < size; i++)
-		ht->array[i] = NULL;
 
-	return (ht);
+	return (table);
 }
