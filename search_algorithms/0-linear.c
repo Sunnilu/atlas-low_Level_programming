610feda803594a9d393f6b2739be9571a781e122
Suntha <Suntha.Lucas@atlasschool.com>
2024-04-16 16:53:36 +0000

    0-linear.c

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,28 @@
+#include "search_algos.h"
+#include <stdio.h>
+
+/**
+ * linear_search - the method used to find an element within the list
+ * @array: the pointer to the first element
+ * @size: the number of elements in the array
+ * @ value: the value to search for
+ * return: -1, if the value is NULL or not present in the array
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	if (array == NULL)
+	{
+		return (-1)
+	}
+	for size_t i = 0; i < size; i++;
+	{
+		 printf("Comparing %d with %d\n", array[i], value);
+
+		 if (array[i] == value)
+		 {
+			 return (i);
+		 }
+	}
+	return (-1);
+}
