69d486b25edcbc4d4499dc6521baf315d96c1cf4
Suntha <Suntha.Lucas@atlasschool.com>
2024-04-16 19:23:35 +0000

    0-linear.c update still not passing

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -11,13 +11,13 @@
 
 int linear_search(int *array, size_t size, int value)
   {
+	size_t i; /* Declare i before the loop */
     if (array == NULL) {
         printf("Value checked array[%lu] = [%d]\n", size, array[size]);
         printf("Array is NULL\n");
         return -1;
     }
 
-    size_t i; /* Declare i before the loop */
     for (i = 0; i < size; i++) {
         printf("Value checked array[%lu] = [%d]\n", i, array[i]);
         if (array[i] == value) {
