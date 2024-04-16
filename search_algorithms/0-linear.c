fe3267e8f0d2a5e82f46b50190e88351eb550d89
Suntha <Suntha.Lucas@atlasschool.com>
2024-04-16 17:29:25 +0000

    removed = from size_t in linear.c

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -15,7 +15,7 @@ int linear_search(int *array, size_t size, int value)
 	{
 		return (-1);
 	}
- 	size_t = i;
+ 	size_t i;
         for (i = 0; i < size; i++)
 	{
 		 printf("Comparing %d with %d\n", array[i], value);
