3451af59475a9189e5aa6761584cb21759843133
Suntha Lucas <155491378+Sunnilu@users.noreply.github.com>
2024-04-15 20:13:42 -0500

    search algos.h

---
+++ search_algos.h
@@ -0,0 +1,36 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+struct listint_s - singly linked list
+* @n: integer
+* @index: index of the node in the list
+* @next: pointer to the next node
+*/
+typedef struct listint_s
+{
+    int n;
+    size_t index;
+    structlistint_s *next;
+}listint_t
+
+/**
+* structs skiplist_s - singly linked list with an express lane
+* @n: integer
+* @ index: indexf thenode list
+* @next: pointer to the next node
+* @express: pointerto thenext node in the express lane
+*/
+typedef struct skiplist_s
+{
+    int n;
+    size_t index;
+    struct skiplist_s *next;
+    struct skiplist_s *express;
+}skiplist_s
+
+
