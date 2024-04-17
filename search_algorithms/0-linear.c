0e87e6c8e354dfdc16dd9cf3f988f45648497f1e
Suntha <Suntha.Lucas@atlasschool.com>
2024-04-17 20:59:11 +0000

    0-linear.c

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -13,24 +13,19 @@
 
 int linear_search(int *array, size_t size, int value) 
 { 
-    size_t i;
+    int i;
 
-    if (array == NULL) 
-        return (-1);
-    
+    if (array == NULL)
+	    return (-1);
 
-    for (i = 0; i < size; i++) 
-    {
-        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-        if (array[i] == value) 
-        {
-            printf("Found %d at index: %lu\n", value, i);
-            return (i); 
-        }
-    }
-
-    
-    return (-1); 
+    for (i == 0; i < (int)size; i++)
+		{
+			printf("value checked array[%u] = [%d]\n, i, array[i]);
+			if (value == array[i])
+				return (i);
+        }   
+    		return (-1);
 }
+
 
 
