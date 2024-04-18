a8d2986ea7e1c467c99afa0a214eb18378f26ca2
Suntha <Suntha.Lucas@atlasschool.com>
2024-04-18 13:16:11 +0000

    tests files

--- tests/1-main.c
+++ tests/1-main.c
@@ -9,12 +9,13 @@
 int main(void)
 {
 	int array[] = { 
-		10, 1, 42, 3, 4, 42, 6, 7, -1, 9
+		0, 1, 2, 3, 4, 5, 6, 7, 8, 9
 	};
 	size_t size = sizeof(array) / sizeof(array[0]);
 
-	printf("found %d at index: %d\n\n", 3, linear_search(array, size, 3));
-	printf("found %d at index: %d\n\n", 42, linear_search(array, size, 42));
+	printf("found %d at index: %d\n\n", 2, linear_search(array, size, 2));
+	printf("found %d at index: %d\n\n", 5, linear_search(array, size, 5, 5));
 	printf("found %d at index: %d\n", 999, linear_seach(array, size, 999));
 	return (EXIT_SUCCESS);
 }
+
