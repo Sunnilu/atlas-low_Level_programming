8b93fb0c250005198330e517414d9577ea9c771e
Suntha <Suntha.Lucas@atlasschool.com>
2024-04-18 13:20:37 +0000

    102 tests file

---
+++ tests/102-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include  "../search_algros.h"
+
+/**
+ * main - entry point
+ * return: always EXIT_SUCCESS
+ */
+int main(void)
+{
+	int array[] = { 
+		0, 0, 1, 2, 2, 2, 2, 3, 3, 4, 4, 5, 6, 6, 7, 8, 8, 8, 9, 9
+	};
+	size_t size = sizeof(array) / sizeof(array[0]);
+
+	printf("found %d at index: %d\n\n", 3, linear_search(array, size, 3));
+	printf("found %d at index: %d\n\n", 7, linear_search(array, size, 7));
+	printf("found %d at index: %d\n", 999, linear_seach(array, size, 999));
+	return (EXIT_SUCCESS);
+}
+
